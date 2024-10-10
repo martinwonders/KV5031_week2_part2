176b1bfcd59bf35a6c71ac31702dc93346acc52d
Endeavour <martin.wonders@unn.ac.uk>
2024-10-10 20:27:47 +0100

    Pyside6 window with a button Exercise 3

--- KV5031_week2_part2.py
+++ KV5031_week2_part2.py
@@ -1,16 +1,31 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QPushButton
+
 
 class HelloWorldWindow(QWidget):
     def __init__(self):
         super().__init__()
 
+        #Create Label and Button
         self.label = QLabel("<b><font color='red'>Martin A Wonders!</font></b>")
+        self.button = QPushButton("Change the label!")
+
+        #Connect button click to the slot (method)
+        self.button.clicked.connect(self.on_button_clicked)
+
+        # Set up layout
         layout = QVBoxLayout()
         layout.addWidget(self.label)
+        layout.addWidget(self.button)
         self.setLayout(layout)
+
         self.setWindowTitle("Hello World App!")
         self.resize(300, 100)
+
+    # Slot (method) for handling button click
+    def on_button_clicked(self):
+        print("Clicked")
+        self.label.setText("<b><font color='blue'>You clicked the button</font></b>")
 
 app = QApplication(sys.argv)
 window = HelloWorldWindow()
