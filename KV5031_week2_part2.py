e94c5dd8e4dd597e7374f9e4e0d2c9487943d550
Endeavour <martin.wonders@unn.ac.uk>
2024-10-10 20:16:43 +0100

    Basic Pyside6 window

---
+++ KV5031_week2_part2.py
@@ -0,0 +1,18 @@
+import sys
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+
+class HelloWorldWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.label = QLabel("Hello World!")
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+        self.setWindowTitle("Hello World App!")
+        self.resize(300, 100)
+
+app = QApplication(sys.argv)
+window = HelloWorldWindow()
+window.show()
+sys.exit(app.exec())
